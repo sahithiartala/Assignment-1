ba427538289718d4aeb7902f956ff4d3c9e9cd2c
sahithiartala <60410833+sahithiartala@users.noreply.github.com>
2020-01-31 10:32:53 -0800

    changes in html and css files

--- public/App.js
+++ public/App.js
@@ -1,19 +1,9 @@
 const element = React.createElement("div", {
-  class: "main"
-}, React.createElement("div", {
   class: "wrapper"
-}, React.createElement("div", {
-  class: "boxed"
-}, "SAHITHI ARTALA"), React.createElement("div", {
-  class: "picture"
-}, React.createElement("img", {
+}, React.createElement("h3", null, "SAHITHI ARTALA"), React.createElement("img", {
   src: "sahithi.jpg",
   alt: "picture"
-})), React.createElement("p", {
-  class: "description"
-}, "Hi my name is sahithi, I am a computer science student masters student at san diego state university.My hobbies are listening to music,playing badminton and painting. I love to travel and explore different countries"), React.createElement("div", {
-  class: "button"
-}, React.createElement("button", {
-  onclick: "window.location.href = 'https://github.com/';"
-}, "VIEW MY GITHUB REPO"))));
+}), React.createElement("p", null, "Hi my name is sahithi, I am a computer science  masters student at san diego state university.My hobbies are listening to music,playing badminton and painting. I love to travel and explore different countries."), React.createElement("button", null, " ", React.createElement("a", {
+  href: "https://github.com/sahithiartala/Assignment-1.git"
+}, "VIEW MY GITHUB REPO")));
 ReactDOM.render(element, document.getElementById('contents'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 const element = (
-<div class="main">
+
 			<div class="wrapper">
-				<div class="boxed">
-				SAHITHI ARTALA</div>
-				<div class="picture">
+				<h3>
+				SAHITHI ARTALA</h3>
 				<img src="sahithi.jpg" alt="picture">
 				</img>
-				</div>
-				<p class="description">Hi my name is sahithi, I am a computer science student masters student at san diego state university.My hobbies are listening to music,playing badminton and painting. I love to travel and explore different countries
+				<p>Hi my name is sahithi, I am a computer science  masters student at san diego state university.My hobbies are listening to music,playing badminton and painting. I love to travel and explore different countries.
 				</p>
-				<div class="button">
-				<button> <a href="https://github.com/sahithiartala/Assignment-1.git"></a>VIEW MY GITHUB REPO</button>
-				</div>
+				<button> <a href="https://github.com/sahithiartala/Assignment-1.git">VIEW MY GITHUB REPO</a></button>
+				
 			</div>
-		</div>
+			
   );
   
   ReactDOM.render(element, document.getElementById('contents'));
